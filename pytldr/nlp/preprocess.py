19076a203a4f4e63ef893f67761fde0ffa8500af
jaijuneja <jai.juneja@gmail.com>
2015-03-07 02:01:57 +0000

    added parse_input as a public function in preprocess

--- pytldr/nlp/preprocess.py
+++ pytldr/nlp/preprocess.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unicodedata
+from goose import Goose
 
 
 def unicode_to_ascii(unicodestr):
@@ -7,5 +8,25 @@ def unicode_to_ascii(unicodestr):
         return unicodestr
     elif isinstance(unicodestr, unicode):
         return unicodedata.normalize('NFKD', unicodestr).encode('ascii', 'ignore')
+    else:
+        raise ValueError('Input text must be of type str or unicode.')
+
+
+def parse_input(text):
+    if isinstance(text, str) or isinstance(text, unicode):
+        if text.startswith('http://'):
+            # Input is a link - need to extract the text from html
+            urlparse = Goose()
+            article = urlparse.extract(url=text)
+            return unicode_to_ascii(article.cleaned_text)
+        elif text.endswith('.txt'):
+            # Input is a file - need to read it
+            textfile = open(text, 'rb')
+            article = textfile.read()
+            textfile.close()
+            return unicode_to_ascii(article)
+        else:
+            # Input is a string containing the raw text
+            return unicode_to_ascii(text)
     else:
         raise ValueError('Input text must be of type str or unicode.')
--- pytldr/summarize/baseclass.py
+++ pytldr/summarize/baseclass.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from goose import Goose
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.preprocessing import normalize
-from ..nlp.preprocess import unicode_to_ascii
+from ..nlp.preprocess import parse_input
 from ..nlp.tokenizer import Tokenizer
 
 
@@ -46,23 +45,7 @@ class BaseSummarizer(object):
 
     @classmethod
     def _parse_input(cls, text):
-        if isinstance(text, str) or isinstance(text, unicode):
-            if text.startswith('http://'):
-                # Input is a link - need to extract the text from html
-                urlparse = Goose()
-                article = urlparse.extract(url=text)
-                return unicode_to_ascii(article.cleaned_text)
-            elif text.endswith('.txt'):
-                # Input is a file - need to read it
-                textfile = open(text, 'rb')
-                article = textfile.read()
-                textfile.close()
-                return unicode_to_ascii(article)
-            else:
-                # Input is a string containing the raw text
-                return unicode_to_ascii(text)
-        else:
-            raise ValueError('Input text must be of type str or unicode.')
+        return parse_input(text)
 
     @classmethod
     def _parse_summary_length(cls, length, num_sentences):
