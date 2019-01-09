92c11f3e44dd7c3808960524c6a52285b4546b42
VinodNimbalkar <omvinod@yahoo.com>
2019-01-09 17:14:34 +0530

    module dependency version updated

--- setup.py
+++ setup.py
@@ -54,13 +54,11 @@ setup(
         'Programming Language :: Python'
     ],
     install_requires=[
-        'numpy==1.8.0',
-        'nltk==2.0.5',
-        'scipy==0.13.2',
-        'scikit-learn==0.15.2',
-        'goose-extractor==1.0.25',
-        'newspaper==0.0.9.8',
-        'networkx==1.9.1'
+        'numpy==1.14.3',
+	    'nltk==3.4',
+	    'scipy==1.1.0',
+	    'scikit-learn==0.20.1'
+        'networkx==2.2'
     ],
     include_package_data=True,
     package_data={PACKAGE_NAME: ['stopwords/*.txt'],
